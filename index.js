b1fc42a9324acbed81a1c1d4805672280981e453
Shreyaan <shreyaans41@gmail.com>
2021-11-18 03:53:46 +0530

    added local sotrage

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 let saveEl = document.getElementById("save-el")
+let oldtext = localStorage.getItem('saveEl')
+saveEl.innerHTML = oldtext
+if(oldtext == null){
+    saveEl.innerHTML = "Previous entires: ";
+}
+
+else{
+    saveEl.innerHTML = oldtext
+
+}
 
 function increment() {
     count += 1
     countEl.innerText = count
 }
-let i = 1;
+let i ;
+
+if(localStorage.getItem('number') != null ){
+i= localStorage.getItem('number')
+}
+else{
+i = 1;
+}
 
 function bruhclear() {
     saveEl.innerHTML = "Previous entires: ";
@@ -22,7 +39,6 @@ function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
     // 3. Render the variable in the saveEl using innerText
     // NB: Make sure to not delete the existing content of the paragraph
-
     if (count == 0) {
 
         if (i == 1) {
@@ -47,4 +63,8 @@ function save() {
     }
     count = 0;
     countEl.innerText = count
+    localStorage.setItem('saveEl' , saveEl.innerHTML );
+    localStorage.setItem('number' , i );
 }
+
+// localStorage.setItem('saveEl' , saveEl.innerHTML );
