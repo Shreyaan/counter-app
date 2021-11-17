37f35d1cb68049cc4dbe4bbec90fbce2900f5a67
Shreyaan <shreyaans41@gmail.com>
2021-11-17 19:29:32 +0530

    1

--- index.js
+++ index.js
@@ -16,4 +16,29 @@ function reset() {
     count = 0
     countEl.innerText = count
 }
+let i = 1;
+
+function hatetrb() {
+    // count = 0
+  
+i++;  
+
+    if (i%2 == 0) {
+        document.getElementById("hatetrb").innerText = 'hate trb'
+    }
+    else
+    {
+
+        document.getElementById("hatetrb").innerHTML = ' <span style="visibility: hidden;"> hatetrb </span>'
+    }
+    
+
+    // countEl.innerText = "hate trb"
+}
+
+
+function newFunction(i) {
+    i++
+    return i
+}
 
