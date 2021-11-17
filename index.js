3d9b16c8dbbf58d2ce8c559a1da5fb04d9129771
Shreyaan <shreyaans41@gmail.com>
2021-11-17 18:48:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// document.getElementById("count-el").innerText = 5
+
+// change the count-el in the HTML to reflect the new count
+
+// camelCase
+let countEl = document.getElementById("count-el")
+
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+function reset() {
+    count = 0
+    countEl.innerText = count
+}
+
